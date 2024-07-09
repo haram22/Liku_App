421cd2ec051a239794d8b09c7bbeee8ec50efc90
haram22 <kimhr9889@handong.ac.kr>
2024-07-09 16:56:03 +0900

    [Feat] add colors

---
+++ lib/Theme/Colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const primaryRed = Color(0xffB5402E);
+const primaryOrange = Color(0xffDC5C38);
+const primaryPurple = Color(0xff6D3C86);
+const subPurple = Color(0xff854493);
+const primaryYellow = Color(0xffEEBB4C);
+const primaryBlue = Color(0xff9290F8);
+const primaryBlack = Color(0xff302A2C);
+const primaryGray = Color(0xffDBDAD4);
