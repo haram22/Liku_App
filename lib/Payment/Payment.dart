bbb7ff21cd748cb4c58af7f3896002aeaf801d21
haram22 <kimhr9889@handong.ac.kr>
2024-08-03 01:33:51 +0900

    [Feat] payment done (gif 제외)

--- lib/Payment/Payment.dart
+++ lib/Payment/Payment.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:liku/Theme/Colors.dart';
-
-import '../Components/Comp.dart';
 import '../Components/TopBottomComp.dart';
 
 class PaymentPage extends StatefulWidget {
@@ -13,38 +11,84 @@ class PaymentPage extends StatefulWidget {
 
 class _PaymentPageState extends State<PaymentPage> {
   @override
-  int select = -2;
-
-  @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: const Headercomp(text: '티켓 수량과 좌석을 선택하세요.'),
+      appBar: const Headercomp(text: '카드를 인식시켜주세요'),
       body: Row(
         children: [
-          const Flexible(flex: 2, child: ShowInfo()),
           Flexible(
             flex: 8,
             child: Column(
               children: [
                 Container(
-                  color: primaryBlack,
+                  color: primaryPurple,
                   width: double.infinity,
                   height: 80,
                   child: Center(
                     child: RichText(
                       textAlign: TextAlign.center,
                       text: const TextSpan(children: [
-                        TextSpan(
-                            text: "티켓 내역 확인하기", style: TextStyle(fontSize: 40))
+                        TextSpan(text: "카드 결제", style: TextStyle(fontSize: 40))
                       ]),
                     ),
                   ),
                 ),
-                Container(
-                  width: double.infinity,
-                  height: 5,
-                  color: Colors.black,
+                Padding(
+                  padding: const EdgeInsets.only(top: 50.0),
+                  child: Center(
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: const TextSpan(children: [
+                        TextSpan(
+                            text: "결제하실 카드",
+                            style: TextStyle(
+                                fontSize: 35,
+                                color: primaryRed,
+                                fontWeight: FontWeight.bold)),
+                        TextSpan(
+                            text: "를 리더기에 삽입해주세요.",
+                            style: TextStyle(
+                                fontSize: 35,
+                                color: primaryBlack,
+                                fontWeight: FontWeight.bold)),
+                      ]),
+                    ),
+                  ),
                 ),
+                SizedBox(height: 30),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: primaryGray, width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  width: screenWidth / 1.5,
+                  height: 70,
+                  child: Center(
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(children: [
+                        TextSpan(
+                            text: "일부 직불카드 및 선불형 카드",
+                            style:
+                                TextStyle(color: primaryBlack, fontSize: 25)),
+                        TextSpan(
+                            text: "의 사용이 ",
+                            style: TextStyle(
+                                color: Colors.grey[400], fontSize: 25)),
+                        TextSpan(
+                            text: "불가능",
+                            style:
+                                TextStyle(color: primaryBlack, fontSize: 25)),
+                        TextSpan(
+                            text: "할 수 있으므로 이용시 참고바랍니다.",
+                            style: TextStyle(
+                                color: Colors.grey[400], fontSize: 25))
+                      ]),
+                    ),
+                  ),
+                )
               ],
             ),
           )
